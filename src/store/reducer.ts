f0ca9c3e5bf87e30b8d034e80cd6b419997573ed
Christik <christik.lucid@gmail.com>
2022-11-09 19:03:44 +0300

    Добавляет action для сброса игры

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit/dist/createReducer';
 import { FIRST_GAME_STEP } from '../const';
-import { incrementStep } from './action';
+import { incrementStep, resetGame } from './action';
 
 const STEP_COUNT = 1;
 
@@ -13,5 +13,9 @@ export const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(incrementStep, (state) => {
       state.step += STEP_COUNT;
+    })
+    .addCase(resetGame, (state) => {
+      state.mistakes = 0;
+      state.step = FIRST_GAME_STEP;
     });
 });
