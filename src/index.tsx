25c16c7c8a0b2b6fd4ac112dedf77022ff820576
Christina Yanovich <christik.lucid@gmail.com>
2022-11-15 15:22:16 +0300

    Добавляет обработку ошибок, приходящих с сервера

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from './store';
+import { checkAuthAction } from './store/api-actions';
 
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,20 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import { processErrorHandle } from './process-error-handle';
+
 import { getToken } from './token';
 
 const BACKEND_URL = 'https://11.react.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) =>
+  !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -19,6 +31,17 @@ export const createAPI = (): AxiosInstance => {
 
     return config;
   });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{error: string}>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error);
+      }
+
+      throw error;
+    }
+  );
 
   return api;
 };
